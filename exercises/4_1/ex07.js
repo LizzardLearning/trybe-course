f520690886ed6c086b80d7e125ed5045bf9b3127
Luis Medeiros <luis.codemaker@gmail.com>
2020-05-12 18:02:44 -0300

    Exercise 7 done

---
+++ exercises/4_1/ex07.js
@@ -0,0 +1,9 @@
+let percent = 60;
+
+if(percent > 100 || percent < 0) console.log("Error!");
+else if(percent >= 90) console.log("a");
+else if(percent >= 80) console.log("b");
+else if(percent >= 70) console.log("c");
+else if(percent >= 60) console.log("d");
+else if(percent >= 50) console.log("e");
+else console.log("f");
