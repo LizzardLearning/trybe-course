0afccecc9971fdc4e076a3c990b1c26454f99933
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-05-15 18:57:21 -0300

    Ex01_p2 Done

--- exercises/4_4/ParteII/ex01.js
+++ exercises/4_4/ParteII/ex01.js
@@ -1,5 +1,18 @@
 function isPalindrome(str){
-  console.log(str.length);
+
+  if(str.length % 2 == 0 || typeof(str) != "string") return false;
+  str = str.toLowerCase();
+
+  let middle_letter_index = Math.floor(str.length/2);
+  let first_part = str.substring(0, middle_letter_index);
+  let last_part = str.substring(middle_letter_index+1);
+  
+  let first_part_index = 0;
+  for(let i=last_part.length-1; i>0; i--){
+    if(last_part[i] != first_part[first_part_index]) return false;
+    first_part_index++;
+  }
+  return true;
 }
 
-isPalindrome("luis");
+console.log(isPalindrome("arara"));
