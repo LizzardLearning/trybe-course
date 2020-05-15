413a409d251343ef8288fadde98c31c07fac9c76
Lizzard <luis.codemaker@gmail.com>
2020-05-15 15:39:28 -0300

    Adding coments to code

--- exercises/4_3/ex05_bonus.js
+++ exercises/4_3/ex05_bonus.js
@@ -1,9 +1,18 @@
+//Declara as variáveis básicas
 let n=5;
 let text = "";
+
+//Calcula o tamanho da mediana da linha para identificar a coluna central
 let median = Math.floor(n/2);
+
+/*
+**  Imprime o número de colunas que coincide com n asteríscos
+**  para isso, uso como N a mediana somado a 2
+*/
 
 for (i=0; i<median+2; i++) {
   for (j=0; j<n; j++){
+    //para imprimir as linhas, uso a mediana como referência
     if(i==1 || i==median+1){
       if(j<=median-i || j>=median+i){
         text += " ";
@@ -18,6 +27,7 @@ for (i=0; i<median+2; i++) {
       }
     }
   }
+  //Imprime cada linha já formatada
   console.log(text);
   text = "";
 }
