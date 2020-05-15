49d33d3ad01c36c2d7a2d2aecb16990627fb7b53
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-05-15 19:34:58 -0300

    Ex05_p2 Done

--- exercises/4_4/ParteII/ex05.js
+++ exercises/4_4/ParteII/ex05.js
@@ -2,6 +2,7 @@ function mustCommon(array){
   if(array.length == 0) return -1;
 
   let caracters = {};
+  let winner = array[0];
 
   for(index in array){
     if(!caracters.hasOwnProperty(array[index])){
@@ -9,7 +10,14 @@ function mustCommon(array){
     }
     caracters[array[index]]++;
   }
-  return caracters;
+
+  for(index in caracters){
+    if(caracters[winner] < caracters[index]){
+      winner = index;
+    }
+  }
+
+  return winner;
 }
 
 console.log(mustCommon([2, 3, 2, 5, 8, 2, 3]));
