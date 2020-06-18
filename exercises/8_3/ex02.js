431c944745d11662ecaf58174b608d2f1b627fdb
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-06-18 18:43:24 -0300

    ex02 done

--- exercises/8_3/ex02.js
+++ exercises/8_3/ex02.js
@@ -1,12 +1,10 @@
 const assert = require('assert')
 
-const rectangleArea = (width, height) => width * height;
+// escreva sum abaixo
+const sum = (...numbers) => numbers.reduce((total, num) => total + num); 
 
-const rectangle1 = [1, 2]
-const rectangle2 = [3, 5]
-const rectangle3 = [6, 9]
-const rectangles = [rectangle1, rectangle2, rectangle3]
-
-for(rectangle of rectangles) {
-    assert.equal(rectangleArea(...rectangle), rectangle[0]*rectangle[1]) // altere a chamada da funcao rectangleArea
-}
+assert.equal(sum(), 0)
+assert.equal(sum(1), 1)
+assert.equal(sum(1, 2), 3)
+assert.equal(sum(1, 2, 3), 6)
+assert.equal(sum(1, 2, 3, 4), 10)
