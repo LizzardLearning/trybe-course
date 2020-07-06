69089eb0c3829ba46d5f9bf38a87039a9a06345c
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-07-06 18:27:12 -0300

    Ex.01 and Ex.02 done

---
+++ exercises/10_2/random.js
@@ -0,0 +1,4 @@
+const {floor, random} = Math;
+const randomNumber = () => floor(random() * 100);
+
+module.exports = randomNumber;
---
+++ exercises/10_2/random.test.js
@@ -0,0 +1,21 @@
+const random = require('./random');
+
+test('Testando random()', () => {
+  /* Exercise 01 */
+  random.randomNumber = jest.fn().mockReturnValue(10);
+
+  random.randomNumber();
+
+  expect(random.randomNumber).toHaveBeenCalledTimes(1);
+  expect(random.randomNumber()).toBe(10);
+
+  /* Exercise 02 */
+  random.randomNumber.mockReset();
+  random.randomNumber = jest.fn().mockImplementation((a, b) => (a/b));
+
+  random.randomNumber();
+
+  expect(random.randomNumber).toHaveBeenCalledTimes(1);
+  expect(random.randomNumber(10, 5)).toBe(2);
+
+});
