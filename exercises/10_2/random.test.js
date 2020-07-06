bcce2ea24caf3c126edeffcd6d875973c08757da
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-07-06 18:36:07 -0300

    Ex.03 done

--- exercises/10_2/random.test.js
+++ exercises/10_2/random.test.js
@@ -11,11 +11,28 @@ test('Testando random()', () => {
 
   /* Exercise 02 */
   random.randomNumber.mockReset();
-  random.randomNumber = jest.fn().mockImplementation((a, b) => (a/b));
+  random.randomNumber = jest.fn().mockImplementation((a, b) => (a / b));
 
   random.randomNumber();
 
   expect(random.randomNumber).toHaveBeenCalledTimes(1);
   expect(random.randomNumber(10, 5)).toBe(2);
+
+  /* Exercise 03 */
+  random.randomNumber.mockReset();
+  random.randomNumber = jest.fn().mockImplementation((a, b, c) => (a * b * c));
+
+  random.randomNumber();
+
+  expect(random.randomNumber).toHaveBeenCalledTimes(1);
+  expect(random.randomNumber(2, 3, 5)).toBe(30);
+
+  random.randomNumber.mockReset();
+  random.randomNumber = jest.fn().mockImplementation((a) => (a * 2));
+
+  random.randomNumber();
+
+  expect(random.randomNumber).toHaveBeenCalledTimes(1);
+  expect(random.randomNumber(15)).toBe(30);
 
 });
